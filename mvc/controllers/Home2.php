0a3ef5b56f3d80a895e5f7bbca66c64d8e4e5619
unknown <tong.congdanh@gmail.com>
2020-12-20 22:41:44 +0700

    add MDB template

---
+++ mvc/controllers/Home2.php
@@ -0,0 +1,27 @@
+<?php 
+    class Home2 extends Controller {
+        function SayHi() {
+            // Các action trong này quy định về việc lấy database từ bảng nào, view nào sẽ lấy ra để hiển thị
+
+            // Model
+            $teo = $this->model("SinhVienModel");
+
+            $this->view("layout_2", [
+                "pageTitle" => "PHC Music & Performing Arts Academy"
+                ]);
+            // View
+        }
+
+        function Show() {
+            $teo = $this->model("SinhVienModel");
+            $sv = $teo->GetSV();
+        
+            $this->view("layout_2", [
+                "Page" => "contact",
+                "SV" => $teo->SinhVien(),
+                "number" => $sv
+                ]);
+
+        }
+    }
+?>
